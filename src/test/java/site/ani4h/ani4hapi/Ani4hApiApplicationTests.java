01499dea97457ca4943af64e4657443a902bd19e
caovanhoang63 <22520457@gm.uit.edu.vn>
2025-02-19 22:04:59 +0700

    Initial commit

---
+++ src/test/java/site/ani4h/ani4hapi/Ani4hApiApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ani4h.ani4hapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ani4hApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
